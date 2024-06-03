a9ecbd56a20aa4f4f0cdf1865dc768f5b98810b4
dianacho <piaristar@gmail.com>
2024-06-03 15:04:49 +0900

    Merge branch 'main' of https://github.com/piaris/yeogiyo

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ import os
 import warnings
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-from streamlit_modal import Modal
+import matplotlib.pyplot as plt
 from datetime import time
 import requests
 import json
