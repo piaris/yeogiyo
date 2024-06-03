5f69237a503794b649e553769523fdcac6d953c8
dianacho <piaristar@gmail.com>
2024-06-03 10:20:04 +0900

    6/3 requirement added

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os
 import warnings
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from streamlit_modal import Modal
 from datetime import time
 import requests
