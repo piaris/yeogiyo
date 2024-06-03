762116e84d30544b55aff6f011c941f99fe10824
유성민 <68213803+YUSEONGMIN@users.noreply.github.com>
2024-06-03 08:43:56 +0000

    .

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from streamlit_modal import Modal
 from streamlit_extras import buy_me_a_coffee
-from streamlit_extras import buy_me_a_coffee
 from datetime import time
 import requests
 import json
