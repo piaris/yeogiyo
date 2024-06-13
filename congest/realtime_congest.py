fcde46d287083628f188ff21f79613a8dec37d4c
kimppu <tovi0907@naver.com>
2024-06-13 14:33:48 +0900

    add naver_crawling.py : 네이버 크롤링 모듈 추가 revise realtime_congest.py : streamlit 라이브러리 추가 및 실행 코드 수정

--- congest/realtime_congest.py
+++ congest/realtime_congest.py
@@ -2,37 +2,38 @@ from io import StringIO
 import json
 import requests
 from statistics import mean
+import streamlit as st
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 # '붐빔' 지역 area_nm 가져오기
 def get_congestArea_data() :
-  url = "https://data.seoul.go.kr/SeoulRtd/getCategoryList?page=1&category=%EC%A0%84%EC%B2%B4%EB%B3%B4%EA%B8%B0&count=15&sort=true"
-  header = {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-    "referer" : "https://data.seoul.go.kr/SeoulRtd/list"
-  }
+    url = "https://data.seoul.go.kr/SeoulRtd/getCategoryList?page=1&category=%EC%A0%84%EC%B2%B4%EB%B3%B4%EA%B8%B0&count=15&sort=true"
+    header = {
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "referer" : "https://data.seoul.go.kr/SeoulRtd/list"
+    }
 
-  congest_area = []
+    congest_area = []
 
-  try :
-    response = requests.get(url, headers=header)
-    response_data = json.loads(response.text)
-    congest_data=response_data['row'][:5]
-    for data in congest_data:
-        congest_lv = data['area_congest_lvl']
-        area = data['area_nm']
-        if congest_lv == '붐빔' :
-          yield area
-  except Exception as e:
-      print(f"페이지의 정보를 가져올 수 없습니다. :{e}")
+    try :
+        response = requests.get(url, headers=header)
+        response_data = json.loads(response.text)
+        congest_data=response_data['row'][:5]
+        for data in congest_data:
+            congest_lv = data['area_congest_lvl']
+            area = data['area_nm']
+            if congest_lv == '붐빔' :
+                yield area
+    except Exception as e:
+        st.error(f"페이지의 정보를 가져올 수 없습니다.: {e}")
 
 
 
 # '붐빔'지역의 'area_nm' 출력
 def print_congestArea() :
     for area in get_congestArea_data():
-        print(area)
+            st.write(area)
 
 
 
@@ -53,9 +54,9 @@ def get_congestRoad_data():
             # result.append({area_nm : avg_spd})
             print(f'{area_nm}:{avg_spd} km/h')
     except requests.exceptions.RequestException as e:
-        print(f"HTTP 요청 오류: {e}")
+        st.error(f"HTTP 요청 오류: {e}")
     except json.JSONDecodeError as e:
-        print(f"JSON 디코딩 오류: {e}")
+        st.error(f"JSON 디코딩 오류: {e}")
 
 
 
@@ -103,4 +104,4 @@ def print_congestRoad():
     results = get_congestRoad_data_parallel()
     for result in results:
         for key, value in result.items():
-            print(f"{key} : {value} km/h")
+            st.write(f"{key} : {value} km/h")
